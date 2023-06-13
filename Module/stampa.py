141f99b67315a8bbc6c73048aab3bb663a0b8ddf
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2023-06-13 10:07:12 +0200

    Update Stampa

--- Module/stampa.py
+++ Module/stampa.py
@@ -1,3 +1,3 @@
 def Printing(x,i):
-    string = str(i)+') '+ x.nome + ':' + str(x.punteggio) + ' (' + str(x.tornei) + ' tornei)'
+    string = f"{str(i)}){x.nome}: {str(x.punteggio)} ({str(x.tornei)} tornei)"
     return string
