3dd5689fa3f5be1367e21cc7f18a5222cc727b85
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2023-06-13 10:01:20 +0200

    Update stampa.py

--- Module/stampa.py
+++ Module/stampa.py
@@ -1,13 +1,3 @@
 def Printing(x,i):
     string = str(i)+') '+ x.nome + ':' + str(x.punteggio) + ' (' + str(x.tornei) + ' tornei)'
     return string
-    # f.write(string)
-    # f.write(str(i))
-    # f.write(')')
-    # f.write(' ')
-    # f.write(x.nome)
-    # f.write(':')
-    # f.write(str(x.punteggio))
-    # f.write(' (')
-    # f.write(str(x.tornei))
-    # f.write(' tornei)')
